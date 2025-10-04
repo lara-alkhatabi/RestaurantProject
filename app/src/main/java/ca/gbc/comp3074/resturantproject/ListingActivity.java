7100a58068065f6cbdd6671a9869c144154af06b
Lara Alkhatabi <101461068@georgebrown.ca>
2025-10-04 17:03:57 -0400

    added resturant listing screen with logout button and user details

--- app/src/main/java/ca/gbc/comp3074/resturantproject/ListingActivity.java
+++ app/src/main/java/ca/gbc/comp3074/resturantproject/ListingActivity.java
@@ -1,24 +1,62 @@
 package ca.gbc.comp3074.resturantproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
 
 public class ListingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_listing);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageView userIcon = findViewById(R.id.userIcon);
+        userIcon.setOnClickListener(v -> {
+            Intent intent = new Intent(ListingActivity.this, AboutActivity.class);
+            startActivity(intent);
         });
+        ImageView logoutIcon = findViewById(R.id.logoutIcon);
+        logoutIcon.setOnClickListener(v -> {
+            Intent intent = new Intent(ListingActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        LinearLayout restcard1 = findViewById(R.id.restcard1);
+        restcard1.setOnClickListener(v -> {
+            Intent intent = new Intent(ListingActivity.this, DetailsActivity.class);
+            startActivity(intent);
+        });
+
+        LinearLayout restcard2 = findViewById(R.id.restcard2);
+        restcard2.setOnClickListener(v -> {
+            Intent intent = new Intent(ListingActivity.this, DetailsActivity.class);
+            startActivity(intent);
+        });
+
+        LinearLayout restcard3 = findViewById(R.id.restcard3);
+        restcard3.setOnClickListener(v -> {
+            Intent intent = new Intent(ListingActivity.this, DetailsActivity.class);
+            startActivity(intent);
+        });
+
+        LinearLayout restcard4 = findViewById(R.id.restcard4);
+        restcard4.setOnClickListener(v -> {
+            Intent intent = new Intent(ListingActivity.this, DetailsActivity.class);
+            startActivity(intent);
+        });
+
+        LinearLayout restcard5 = findViewById(R.id.restcard5);
+        restcard5.setOnClickListener(v -> {
+            Intent intent = new Intent(ListingActivity.this, DetailsActivity.class);
+            startActivity(intent);
+        });
+
+
+
     }
 }
