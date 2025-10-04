672b271f1d6a295f5aadf1e8e04a70e6a9c74f96
Lara Alkhatabi <101461068@georgebrown.ca>
2025-10-04 18:30:11 -0400

    updated all files and making sure everything works

--- app/src/main/java/ca/gbc/comp3074/resturantproject/DetailsActivity.java
+++ app/src/main/java/ca/gbc/comp3074/resturantproject/DetailsActivity.java
@@ -1,7 +1,9 @@
 package ca.gbc.comp3074.resturantproject;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +20,22 @@ public class DetailsActivity extends AppCompatActivity {
             Intent intent = new Intent(DetailsActivity.this, ListingActivity.class);
             startActivity(intent);
         });
+
+        Button mapbutton = findViewById(R.id.mapbutton);
+        String mapurl="https://maps.app.goo.gl/snqgunQb54ncKHb48";
+        Uri mapuri = Uri.parse(mapurl);
+        mapbutton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, mapuri);
+            startActivity(intent);
+        });
+        Button sharebutton = findViewById(R.id.sharebutton);
+        String shareurl="https://share.google/dvlzo0786qD36Cu3C";
+        Uri shareuri = Uri.parse(shareurl);
+        sharebutton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, shareuri);
+            startActivity(intent);
+        });
+
 
     }
 }
